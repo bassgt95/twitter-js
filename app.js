0b744ae4e45c3868fb687d39c23c4f0ef068a5d0
Graham Bass <bassgt95@gmail.com>
2018-04-19 11:59:50 -0400

    morgan?

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const twitterApp = express();
 
-twitterApp.get('/', (req, res) => res.send('Hey!'))
+// twitterApp.post('/is-anybody-in-there', function(req, res, next) {
+//     res.send('modernism');
+//     next();
+// });
+
+twitterApp.use(bodyParser.json());
+
+// twitterApp.use(function(req, res) {
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.write('you posted:\n');
+//     res.end(JSON.stringify(req.body, null, 2));
+// });
+
+twitterApp.use(morgan('default'));
+
+twitterApp.get('/', (req, res) => res.send('Hey!'));
+
+// twitterApp.get('/is-anybody-in-there', (req, res) => res.send('Hello?'));
+
+twitterApp.get('/news', function(req, res, next) {
+    res.send('news!!!')
+});
 
 twitterApp.listen('3000', function() {
     console.log('server listening');
