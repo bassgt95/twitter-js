03546e66c0665a58c2b4f4b497b556e192484f63
Graham Bass <bassgt95@gmail.com>
2018-04-19 10:32:20 -0400

    server running!

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
-const app = express();
+const twitterApp = express();
+
+twitterApp.get('/', (req, res) => res.send('Hey!'))
+
+twitterApp.listen('3000', function() {
+    console.log('server listening');
+});
 
